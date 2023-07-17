070656bf9553fd3874dbbc11c65fb50c1f2d0d85
AdnRamos <adnramos108@gmail.com>
2023-07-17 10:26:53 -0300

    iniciado

---
+++ src/test/java/br/edu/ufape/poo/gerenciamentoestudantes/GerenciamentoEstudantesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ufape.poo.gerenciamentoestudantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoEstudantesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
